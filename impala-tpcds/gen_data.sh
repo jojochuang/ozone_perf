f48d67a0222872f36a51f71ba751eb6551474a76
Wei-Chiu Chuang <weichiu@apache.org>
2022-07-14 16:32:15 -0700

    Write TPC-DS input data into /tmp if file system is HDFS.

--- impala-tpcds/gen_data.sh
+++ impala-tpcds/gen_data.sh
@@ -14,17 +14,28 @@ else
     IMPALA_SSL=""
 fi
 
+if [ "$FILE_SYSTEM" == "ozone" ]; then
+    # choose between o3fs and ofs
+    export FILE_SYSTEM_PREFIX="ofs://$OZONE_SERVICE_ID/vol1/bucket"
+    export TPCDS_INPUT_PREFIX="$FILE_SYSTEM_PREFIX"
+elif [ "$FILE_SYSTEM" == "hdfs" ]; then
+    export FILE_SYSTEM_PREFIX=""
+    export TPCDS_INPUT_PREFIX="$FILE_SYSTEM_PREFIX/tmp"
+fi
+
 for s in "${scale[@]}"
 do
 	cd $CURRENT_DIR/impala-tpcds-kit/tpcds-gen
-	yarn jar target/tpcds-gen-1.0-SNAPSHOT.jar -d $FILE_SYSTEM_PREFIX/impala_${FILE_SYSTEM}_${s} -p 25 -s ${s}
+	command="yarn jar target/tpcds-gen-1.0-SNAPSHOT.jar -d $TPCDS_INPUT_PREFIX/impala_${FILE_SYSTEM}_${s} -p 25 -s ${s}"
+	echo $command
+	`$command`
 	cd ../..
 
-        expanded_prefx=$( echo $FILE_SYSTEM_PREFIX | sed 's/\//\\\//g' )
-        echo $expanded_prefx
-        sed "s/\/tmp\/tpc-ds\/sf10000/${expanded_prefx}\/impala_${FILE_SYSTEM}_${s}/g" impala-tpcds-kit/scripts/impala-external.sql  > impala-external-${FILE_SYSTEM}-${s}gb.sql
-        sed "s/\/tmp\/tpc-ds\/sf10000/${expanded_prefx}\/impala_${FILE_SYSTEM}_${s}/g" impala-tpcds-kit/scripts/impala-parquet.sql  > impala-parquet-${FILE_SYSTEM}-${s}gb.sql
-        sed "s/\/tmp\/tpc-ds\/sf10000/${expanded_prefx}\/impala_${FILE_SYSTEM}_${s}/g" impala-tpcds-kit/scripts/impala-insert.sql  > impala-insert-${FILE_SYSTEM}-${s}gb.sql
+        expanded_prefix=$( echo $TPCDS_INPUT_PREFIX | sed 's/\//\\\//g' )
+        echo "expanded_prefix=$expanded_prefix"
+        sed "s/\/tmp\/tpc-ds\/sf10000/${expanded_prefix}\/impala_${FILE_SYSTEM}_${s}/g" impala-tpcds-kit/scripts/impala-external.sql  > impala-external-${FILE_SYSTEM}-${s}gb.sql
+        sed "s/\/tmp\/tpc-ds\/sf10000/${expanded_prefix}\/impala_${FILE_SYSTEM}_${s}/g" impala-tpcds-kit/scripts/impala-parquet.sql  > impala-parquet-${FILE_SYSTEM}-${s}gb.sql
+        sed "s/\/tmp\/tpc-ds\/sf10000/${expanded_prefix}\/impala_${FILE_SYSTEM}_${s}/g" impala-tpcds-kit/scripts/impala-insert.sql  > impala-insert-${FILE_SYSTEM}-${s}gb.sql
 
         sed -i "s/tpcds_10000/tpcds_${FILE_SYSTEM}_${s}/g" impala-external-${FILE_SYSTEM}-${s}gb.sql
         sed -i "s/tpcds_10000/tpcds_${FILE_SYSTEM}_${s}/g" impala-parquet-${FILE_SYSTEM}-${s}gb.sql
